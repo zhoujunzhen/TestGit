416c3c686cddde621c851613e3a72a78fbc3edb6
zhou_glb <zhou_glb@qq.com>
2020-06-12 17:39:23 +0800

    modify main() again

--- src/main/java/com/zhou/git/Happy.java
+++ src/main/java/com/zhou/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("happy main left...");
 		System.out.println("happy main right...");
+		System.out.println("happy main again...");
 	}
 }
