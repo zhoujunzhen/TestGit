a7da95e80587900a663779cc075e4ac1e3c1f515
zhou_eclipse <zhou_eclipse@qq.com>
2020-06-12 18:00:41 +0800

    new branch hot fix

--- src/main/java/com/zhou/git/Happy.java
+++ src/main/java/com/zhou/git/Happy.java
@@ -5,5 +5,6 @@ public class Happy {
 		System.out.println("happy main left...");
 		System.out.println("happy main right...");
 		System.out.println("happy main again...");
+		System.out.println("hot fix...");
 	}
 }
